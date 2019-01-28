54badc3d4e7c667185bee209ff6f55110546da9a
Krzystof Kolacz <kolin4@gmail.com>
2019-01-28 20:04:33 +0100

    test

--- server/movement.js
+++ server/movement.js
@@ -1,5 +1,20 @@
 // engines sterings
+const Gpio = require('pigpio').Gpio;
 
+const forward = new Gpio(21, {mode: Gpio.OUTPUT});
+const backward = new Gpio(20, {mode: Gpio.OUTPUT});
+const pwaForward = new Gpio(16, {mode: Gpio.OUTPUT});
+
+let dutyCycle = 0;
+forward.digitalWrite(1)
+setInterval(() => {
+  pwaForward.pwmWrite(dutyCycle);
+
+  dutyCycle += 5;
+  if (dutyCycle > 255) {
+    dutyCycle = 0;
+  }
+}, 20);
 
 const catchMove = ({x = 0,y = 0}) => {
   console.log('x=' + x)
